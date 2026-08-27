f6aa385be76718a1a86b14a39661cb4fccbc4eae
lmzkjmiajzj-lgtm <lmzkjmiajzj@gmail.com>
2026-08-27 23:08:47 +0100

    Update file.js

--- file.js
+++ file.js
@@ -1,6 +1,5 @@
   var d = parent.document;
 
-  d.querySelectorAll('.fk-d-tooltip__trigger, .fk-d-tooltip, .fk-d-tooltip iframe, iframe[aria-hidden="true"]').forEach(function (e) { e.remove(); });
 
   var a = d.createElement('a');
   a.href = 'https://api.netlify.com/auth?provider=github&site_id=app.netlify.com&tracking_session_id=afc7d48b-e925-432a-a170-9121236eda09&login=true&entry_point=direct&browser_fingerprint=cbdb5739abc754bdcc16cbd4fb07cd0f&device_fingerprint=bbbac72e6221cf088819fff54ca43b40&redirect=https%3A%2F%2fwww.netlify.com%2F&use_redirect=true';
